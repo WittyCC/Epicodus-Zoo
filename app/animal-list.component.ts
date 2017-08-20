6af4a650037fc1edb5acc11eabf2c20ae19a3e99
Witty Chang <wcc1213@gmail.com>
2017-08-20 13:57:17 -0700

    Create new animal-list child component.

---
+++ app/animal-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'animal-list',
+  template: `
+  `
+})
+
+export class AnimalListComponent {
+  
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Epicodus Zoo</h1>
+  <div class="container">
+    <h1>Epicodus Zoo</h1>
+    <h2>Logged Animals for {{month}}/{{day}}/{{year}}</h2>
+  </div>
   `
 })
 
 export class AppComponent {
-  
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+  firstAnimal = {
+    species: "Arctic Fox",
+    name: "Moon",
+    age: 2,
+    diet: "Carnivore",
+    location: "Northern Trail",
+    caretakers: 5,
+    sex: "Female",
+    likes: "Cool shade",
+    dislikes: "Loud noises"
+  }
 }
